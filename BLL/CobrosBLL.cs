3e2aec5f7f1a84f3e7aa891e02876f1058ae60e3
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-30 18:40:42 -0400

    Creando BLL de modelo Cobros.

---
+++ BLL/CobrosBLL.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_ap2_20180619.Models;
+using Parcial2_ap2_20180619.DAL;
+
+namespace Parcial2_ap2_20180619.BLL
+{
+    public class CobrosBLL
+    {
+        public static bool Guardar(Cobros cobros)
+        {
+            if (!Existe(cobros.CobroId))
+                return Insertar(cobros);
+            else
+                return Modificar(cobros);
+        }
+
+        private static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Cobros.Any(e => e.CobroId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        private static bool Insertar(Cobros cobros)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Cobros.Add(cobros);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Cobros cobros)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete from CobrosDetalle where CobroId = {cobros.CobroId}");
+
+                foreach (var item in cobros.Detalle)
+                {
+                    contexto.Entry(item).State = EntityState.Added;
+                }
+
+                contexto.Entry(cobros).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                var eliminado = contexto.Cobros.Find(id);
+
+                if (eliminado != null)
+                {
+                    contexto.Entry(eliminado).State = EntityState.Deleted;
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Cobros Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Cobros cobros;
+
+            try
+            {
+                cobros = contexto.Cobros
+                    .Include(r => r.Detalle)
+                    .ThenInclude(r => r.Venta)
+                    .Include(r => r.Cliente)
+                    .Where(r => r.CobroId == id)
+                    .SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return cobros;
+        }
+
+        public static List<Cobros> GetList(Expression<Func<Cobros, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Cobros> lista = new List<Cobros>();
+
+            try
+            {
+                lista = contexto.Cobros.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
