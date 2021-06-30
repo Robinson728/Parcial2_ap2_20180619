7df76e987363456ac3c8e982cad25574fb622704
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-30 15:24:39 -0400

    Creando Consulta de Clientes.

---
+++ BLL/Conversiones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_ap2_20180619.BLL
+{
+    public class Conversiones
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+
+        public static double ToDouble(string valor)
+        {
+            double retorno;
+
+            double.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
