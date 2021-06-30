b7e53d3d273ee755c8236d8a76d26780a4338252
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-30 14:55:12 -0400

    Creando Modelos Ventas y Clientes.

---
+++ Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_ap2_20180619.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public List<Ventas> Venta { get; set; } = new List<Ventas>();
+    }
+}
---
+++ Models/Ventas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_ap2_20180619.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public Clientes Cliente { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+    }
+}
