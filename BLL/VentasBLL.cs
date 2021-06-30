6297de8369b3b02782ca5b187309e8fdfab4083c
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-30 15:16:49 -0400

    Creando BLL del Modelo Ventas.

---
+++ BLL/VentasBLL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_ap2_20180619.DAL;
+using Parcial2_ap2_20180619.Models;
+
+namespace Parcial2_ap2_20180619.BLL
+{
+    public class VentasBLL
+    {
+        public static Ventas Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Ventas ventas;
+
+            try
+            {
+                ventas = contexto.Ventas.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ventas;
+        }
+
+        public static List<Ventas> GetList(Expression<Func<Ventas, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Ventas> lista = new List<Ventas>();
+
+            try
+            {
+                lista = contexto.Ventas.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
