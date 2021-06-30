d285ace83ae7a6e1bb145de6daf8dd7a7f4b25e8
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-30 18:23:36 -0400

    Creando modelo CobrosDetalle.

---
+++ Models/CobrosDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_ap2_20180619.Models
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CobroId { get; set; }
+        public double Cobrado { get; set; }
+        public bool Pagado { get; set; }
+        public int VentaId { get; set; }
+        public virtual Ventas Venta { get; set; }
+    }
+}
